320367dcbb99b6c3de1846cd38a74b61fb173ca3
Patotricks <patrickufrrj@gmail.com>
2025-03-15 19:13:50 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from typing import Annotated, Sequence, TypedDict
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage, add_messages]]
+
